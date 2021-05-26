f74f2f7efc2301f222fa614d3bbd5c4b36617c46
shafali03 <shaf03sm@gmail.com>
2021-05-26 20:35:10 +0100

    Moved the logic from the route to the controllers posts

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,9 @@
 import express from "express";
 
+import { getPost } from "../controllers/posts.js";
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("THIS WORKS");
-});
+router.get("/", getPost);
 
 export default router;
