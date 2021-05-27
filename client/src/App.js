f656e91a1d819f91475a2a9a6d668896fe3c6787
shafali03 <shaf03sm@gmail.com>
2021-05-27 10:48:05 +0100

    Added styles for the App file

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,16 @@ import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
 import logo from "./image/logo.png";
+import useStyles from "./styles";
 
 const App = () => {
+  const classes = useStyles();
+
   return (
     <Container maxWidth="lg">
-      <AppBar position="static" color="inherit">
+      <AppBar className={classes.AppBar} position="static" color="inherit">
         <Typography variant="h4" align="left">
-          <img src={logo} alt="logo" height="65" />
+          <img className={classes.image} src={logo} alt="logo" height="65" />
         </Typography>
       </AppBar>
       <Grow in>
