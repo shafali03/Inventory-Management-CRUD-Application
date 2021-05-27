48ffca1771e4075e5fd45f3ec9edb9aa2b963287
shafali03 <shaf03sm@gmail.com>
2021-05-27 10:28:57 +0100

    Created Posts, Post, Form component and imported inside App file

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
 import logo from "./image/logo.png";
 
 const App = () => {
@@ -10,6 +12,23 @@ const App = () => {
           <img src={logo} alt="logo" height="65" />
         </Typography>
       </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
     </Container>
   );
 };
---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Post from "./Post/Post";
+
+const Posts = () => {
+  return (
+    <div>
+      <h2>Posts</h2>
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  );
+};
+
+export default Posts;
