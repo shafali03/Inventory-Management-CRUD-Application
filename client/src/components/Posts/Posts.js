7363518ad0f99dd0b0f89e7b39b559cdfae4d090
shafali03 <shaf03sm@gmail.com>
2021-05-31 18:53:37 +0100

    Displaying circularProgress else displayin post with styles

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,19 +1,30 @@
 import React from "react";
+import { Grid, CircularProgress } from "@material-ui/core";
 import { useSelector } from "react-redux";
 import Post from "./Post/Post";
+import useStyles from "./styles";
 
 const Posts = () => {
   const posts = useSelector((state) => state.posts);
+  const classes = useStyles();
 
   console.log(posts);
 
-  return (
-    <div>
-      <h2>Posts</h2>
-      <Post />
-      <Post />
-      <Post />
-    </div>
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItem="stretch"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid key={post._id} item xs={12} sm={6}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
