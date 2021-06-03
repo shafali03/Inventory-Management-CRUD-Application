652e143b0f5ea7e98102b34f665ac9728eeff67c
shafali03 <shaf03sm@gmail.com>
2021-06-03 18:21:13 +0100

    Created action deletePost

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -8,7 +8,7 @@ export const getPosts = () => async (dispatch) => {
 
     dispatch({ type: "FETCH_ALL", payload: data });
   } catch (error) {
-    console.log(error.message);
+    console.log(error);
   }
 };
 
@@ -27,6 +27,16 @@ export const updatePost = (id, post) => async (dispatch) => {
     const { data } = await api.updatePost(id, post);
 
     dispatch({ type: "UPDATE", payload: data });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    await api.deletePost(id);
+
+    dispatch({ type: "DELETE", payload: id });
   } catch (error) {
     console.log(error);
   }
