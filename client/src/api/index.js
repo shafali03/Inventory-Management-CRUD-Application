3a03e54383e88bfcde9f8d91719e6bcd0a26a160
shafali03 <shaf03sm@gmail.com>
2021-06-21 22:10:20 +0100

    Updated API for signIn and signUp

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,6 +2,16 @@ import axios from "axios";
 
 const API = axios.create({ baseURL: "http://localhost:5000" });
 
+API.interceptors.request.use((req) => {
+  if (localStorage.getItem("profile")) {
+    req.headers.Authorization = `Bearer ${
+      JSON.parse(localStorage.getItem("profile")).token
+    }`;
+  }
+
+  return req;
+});
+
 export const fetchPosts = () => API.get("/posts");
 
 export const createPost = (newPost) => API.post("/posts", newPost);
@@ -13,6 +23,6 @@ export const deletePost = (id) => API.delete(`/posts/${id}`);
 
 export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
 
-export const signIn = (formData) => API.post("/users/signin", formData);
+export const signIn = (formData) => API.post("/user/signin", formData);
 
-export const signUp = (formData) => API.post("/users/signUp", formData);
+export const signUp = (formData) => API.post("/user/signUp", formData);
