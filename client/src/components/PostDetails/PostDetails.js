863d34f97db3632005e6b1cf08e417e1c2bc1fec
shafali03 <shaf03sm@gmail.com>
2021-06-27 22:38:42 +0100

    Refactor app.js and created empty postDetails component

---
+++ client/src/components/PostDetails/PostDetails.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PostDetails = () => {
+  console.log("Post Details");
+  return (
+    <div>
+      <h2>Dost Details</h2>
+    </div>
+  );
+};
+
+export default PostDetails;
